ead9b3b30941c17a176c9ffc0ec375dea57d30e8
Emily Walker <combatstarlet@gmail.com>
2016-09-01 17:55:58 +0100

    Completed setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = React.createClass({
+
+            render() {
+                return ( < p > Hello! < /p>)
+                }
+
+            })
+
+        ReactDOM.render( < App / > , document.getElementById('app'));
